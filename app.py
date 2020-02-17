ea67eba9b80225e4f1d4a0200e110bcabdfd3f2e
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-02-17 10:29:39 -0800

    updates to index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request#, session, abort
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
@@ -37,34 +37,34 @@ def index():
 def register():
     return register()
 
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
-    else:
-        flash('wrong password!')
-        #return index()  
+# @app.route('/login', methods=['POST'])
+# def do_admin_login():
+#     if request.form['password'] == 'password' and request.form['username'] == 'admin':
+#         session['logged_in'] = True
+#     else:
+#         flash('wrong password!')
+#         #return index()  
 
-@app.route('/admin')
-@login_required
-def admin():
-    """Admin page to post confession (User)"""
-    return admin()
+# @app.route('/admin')
+# @login_required
+# def admin():
+#     """Admin page to post confession (User)"""
+#     return admin()
 
-@app.route('/begin_auth')
-@login_required
-def begin_auth():
-    return redirect(f"https://slack.com/oauth/authorize?scope={ oauth_scope }&client_id={ client_id }&redirect_uri={network}/finish_auth")
+# @app.route('/begin_auth')
+# @login_required
+# def begin_auth():
+#     return redirect(f"https://slack.com/oauth/authorize?scope={ oauth_scope }&client_id={ client_id }&redirect_uri={network}/finish_auth")
 
-@app.route('/admin/<team_id>')
-@login_required
-def approve_or_deny():
-    return admin()
+# @app.route('/admin/<team_id>')
+# @login_required
+# def approve_or_deny():
+#     return admin()
 
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return index()
+# @app.route("/logout")
+# def logout():
+#     session['logged_in'] = False
+#     return index()
 
 """
 @app.route('/auth')
